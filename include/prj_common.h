94135a717f2f4c89039141bd3837d45f7891cf5a
Ole Bjerkemo <48286739+Solidedge@users.noreply.github.com>
2022-03-05 15:41:56 +0100

    Create prj_common.h

---
+++ include/prj_common.h
@@ -0,0 +1,21 @@
+#ifndef _PRJ_COMMON_H_
+#define _PRJ_COMMON_H_
+
+/** Makros */
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  
+/** Content Area */
+  
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* _PRJ_COMMON_H_ */
