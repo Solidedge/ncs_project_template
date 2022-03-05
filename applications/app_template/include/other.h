25f43bf879b5e3f8676063b29e546afbd09bea31
Ole Bjerkemo <48286739+Solidedge@users.noreply.github.com>
2022-03-05 15:18:18 +0100

    Create other.h

---
+++ applications/app_template/include/other.h
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * @brief Header file
+ */
+
+#ifndef OTHER_H__
+#define OTHER_H__
+
+/** Includes */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/** Content of header file */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MODEL_HANDLER_H__ */
